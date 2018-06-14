fd3a504a07102ee12a022437af8833557402e73b
lubinsu <lubinsu@gmail.com>
2018-06-14 14:14:39 +0800

    streams 示例优化，将原来只是单纯的接收数据改为接收数据，数据处理，数据发送

--- src/main/java/com/maiya/kafka/MyStreams.java
+++ src/main/java/com/maiya/kafka/MyStreams.java
@@ -1,11 +1,13 @@
 package com.maiya.kafka;
 
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.*;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
 
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
@@ -15,17 +17,27 @@ import java.util.concurrent.CountDownLatch;
  * Desc:
  */
 public class MyStreams {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-wordcount");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.0.181:9092,192.168.0.181:9092,192.168.0.181:9092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
         final StreamsBuilder builder = new StreamsBuilder();
 
-        builder.stream("streams-plaintext-input").to("streams-pipe-output");
+        KStream<String, String> source = builder.stream("streams-plaintext-input");
+
+        source.flatMapValues(value -> Arrays.asList(value.toLowerCase(Locale.getDefault()).split(" ")))
+                .selectKey((key, word) -> word)
+                .groupByKey()
+                .count(Materialized.as("counts-store"))
+                .toStream()
+                .map((key, value) -> KeyValue.pair(null, key.concat(", ").concat(String.valueOf(value))))
+                .to("wordcount-topic", Produced.with(null, Serdes.String()));
+
         final Topology topology = builder.build();
+        System.out.println(topology.describe());
         final KafkaStreams streams = new KafkaStreams(topology, props);
         final CountDownLatch latch = new CountDownLatch(1);
 
